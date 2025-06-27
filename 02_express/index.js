c81648cb8433b26eef6c5141e9f78f306012ad6e
Gaurav Purohit <grajpurohittt@gmail.com>
2025-06-27 21:44:24 +0530

    integrated logger and morgan for better logging

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,15 +1,31 @@
-
+import logger from "./logger.js";
+import morgan from "morgan";
 import "dotenv/config";
 import express from "express";
 const app = express()
 
 const port = process.env.PORT || 3000;
-
-
+const morganFormat = ":method :url :status :response-time ms";
 app.use(express.json());
 
 let teaData = [];
 let nextId = 1;
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // add a new tea
 app.post("/teas", (req, res) => {
